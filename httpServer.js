f205ff6e0f1bd12c1a86de8076afc62872a03c8e
Dylan Vu <dylan.n.vu@gmail.com>
2016-12-19 02:19:09 -0800

    add post request handler

--- httpServer.js
+++ httpServer.js
@@ -57,6 +57,68 @@ const server = http.createServer((req, res) => {
       res.end(petJSON);
     });
   }
+  else if (req.method === 'POST' && req.url === '/pets') {
+    let bodyJSON = '';
+
+    req.on('data', (chunk) => {
+      bodyJSON += chunk.toString();
+    });
+
+    req.on('end', () => {
+      fs.readFile(petsPath, 'utf8', (readErr, data) => {
+        if (readErr) {
+          console.error(readErr.stack);
+
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('Internal Server Error');
+
+          return;
+        }
+
+        const body = JSON.parse(bodyJSON);
+        const age = Number.parseInt(body.age);
+        const kind = body.kind;
+        const name = body.name;
+
+        if (Number.isNaN(age) || !kind || !name) {
+          res.statusCode = 400;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('Bad Request');
+
+          return;
+        }
+
+        const pets = JSON.parse(data);
+
+        const pet = {
+          age,
+          kind,
+          name
+        };
+
+        pets.push(pet);
+
+        const petJSON = JSON.stringify(pet);
+        const petsJSON = JSON.stringify(pets);
+
+        fs.writeFile(petsPath, petsJSON, (writeErr) => {
+          if (writeErr) {
+            console.error(writeErr.stack);
+
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Internal Server Error');
+
+            return;
+          }
+
+          res.setHeader('Content-Type', 'application/json');
+          res.end(petJSON);
+        });
+      });
+    });
+  }
   else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
