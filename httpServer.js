97866064e19410bd000a9e9474d540640bf32940
Dylan Vu <dylan.n.vu@gmail.com>
2016-12-19 02:14:25 -0800

    add regex route handler for index

--- httpServer.js
+++ httpServer.js
@@ -27,6 +27,36 @@ const server = http.createServer((req, res) => {
       res.end(data);
     });
   }
+  else if (req.method === 'GET' && petRegExp.test(req.url)) {
+    fs.readFile(petsPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err.stack);
+
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+
+        return;
+      }
+
+      const pets = JSON.parse(data);
+      const matches = req.url.match(petRegExp);
+      const index = Number.parseInt(matches[1]);
+
+      if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not Found');
+
+        return;
+      }
+
+      const petJSON = JSON.stringify(pets[index]);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
+    });
+  }
   else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
