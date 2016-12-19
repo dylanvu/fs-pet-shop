9e26ce4c0d7c688b243399af7a69e525ec1c73a5
Dylan Vu <dylan.n.vu@gmail.com>
2016-12-18 20:36:51 -0800

    add functionality

---
+++ pets.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Import modules
+const fs = require('fs');
+const path = require('path');
+// pointer to pets data
+const petsPath = path.join(__dirname, 'pets.json');
+
+// assign command-line args
+const node = path.basename(process.argv[0]);
+const file = path.basename(process.argv[1]);
+const cmd = process.argv[2];
+
+fs.readFile(petsPath, 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  const pets = JSON.parse(data);
+
+  console.log(pets);
+});
